6b8fadf20e4404c54b026522809c198d0ac4906c
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2020-05-02 08:27:23 -0700

    Updated hover effects to work on mobile. Added CSAI experience.

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,34 +20,15 @@ const IndexPage = () => (
       </div>
 
       <div>
-        <h2 class="category">Teaching & Lab Experience</h2>
-        <h3><a
-            href="https://atom.calpoly.edu/LA/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >Chemistry Teaching Assistant</a></h3>
-        <p>Instructed an undergraduate introductory chemistry course in a lecture-lab studio.</p>
-        <h3><a
-            href="https://chemistry.calpoly.edu/content/faculty/fogle_emily"
-            target="_blank"
-            rel="noopener noreferrer"
-          >Mechanistic Enzymology Lab Technician</a></h3>
-        <p>Characterized a S-NAGS enzyme under the guidance of Dr. Emily Fogle and presented discoveries at 
-          <a
-          href="https://cosam.calpoly.edu/frost-fund"
-          target="_blank"
-          rel="noopener noreferrer"
-          >research conferences</a> at Cal Poly.
-        </p>
-        <h3><a
-            href="https://cesame.calpoly.edu/content/learn-by-doing-lab"
-            target="_blank"
-            rel="noopener noreferrer"
-          >Learn by Doing Lab Instructor</a></h3>
-        <p>Led middle school students in experimental chemistry and biology.</p>
-      </div>
-      <div>
         <h2 class="category">Work & Leadership</h2>
+        <h3><a
+            href="https://www.calpolycsai.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Web Developer, Cal Poly CSAI</a></h3>
+        <p>Computer Science & Artificial Intelligence Club
+          <span class="interval">April 2020 - present</span>
+        </p>
         <h3><a
             href="http://mcsla.org/"
             target="_blank"
@@ -64,6 +45,33 @@ const IndexPage = () => (
         <p>Cal Poly Triathlon Team
           <span class="interval">September 2018 - April 2020</span>
         </p>
+      </div>
+
+      <div>
+        <h2 class="category">Teaching & Lab Experience</h2>
+        <h3><a
+            href="https://atom.calpoly.edu/LA/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Chemistry Teaching Assistant</a></h3>
+        <p>Instructed an undergraduate introductory chemistry course in a lecture-lab studio.</p>
+        <h3><a
+            href="https://chemistry.calpoly.edu/content/faculty/fogle_emily"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Mechanistic Enzymology Lab Technician</a></h3>
+        <p>Characterized a S-NAGS enzyme under the guidance of Dr. Emily Fogle and presented discoveries at <a
+            href="https://cosam.calpoly.edu/frost-fund"
+            target="_blank"
+            rel="noopener noreferrer"
+          >research conferences</a> at Cal Poly.
+        </p>
+        <h3><a
+            href="https://cesame.calpoly.edu/content/learn-by-doing-lab"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Learn by Doing Lab Instructor</a></h3>
+        <p>Led middle school students in experimental chemistry and biology.</p>
       </div>
 
       <div>

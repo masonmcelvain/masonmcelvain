b73c1a4af4f21c6df7ea65500347f384c2617efc
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-08-27 15:40:48 -0700

    prose: Add pronouns

--- templates/home/About.tsx
+++ templates/home/About.tsx
@@ -6,8 +6,9 @@ export function About() {
    return (
       <>
          <Text mt={3}>
-            I&apos;m a 23 year old living in San Luis Obispo. I graduated from
-            Cal Poly with a degree in computer science in March 2022.
+            I&apos;m a 23 year old living in San Luis Obispo (he/him). I
+            graduated from Cal Poly with a degree in computer science in March
+            2022.
          </Text>
          <Heading as="h3" size="lg" my={3}>
             How silicon became my favorite element 🧪

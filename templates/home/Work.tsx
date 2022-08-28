bb1a9a3303351d5155449590862ab066873a41f5
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-08-28 01:40:55 -0700

    work: Update text

--- templates/home/Work.tsx
+++ templates/home/Work.tsx
@@ -1,13 +1,13 @@
 import { Text } from "@chakra-ui/react";
-import { SectionHeading } from "@ui/components/SectionHeading";
+import { SectionHeading } from "@ui/typography";
 
 export function Work() {
    return (
       <>
          <SectionHeading>What I&apos;ve been working on 🛠️</SectionHeading>
          <Text>
-            A deeper dive into some of the projects and companies I&apos;ve
-            devoted my time to.
+            A deeper dive into some of the projects and teams I&apos;ve devoted
+            my time to.
          </Text>
       </>
    );

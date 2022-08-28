d873efed161d9716409b52f948c4a46d077526bc
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-08-28 03:14:32 -0700

    favicon: Add new one

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,7 @@ class MyDocument extends Document {
                   href="https://fonts.googleapis.com/css2?family=Muli&display=optional"
                   rel="stylesheet"
                />
+               <Favicon />
             </Head>
             <body>
                <Main />
@@ -20,3 +21,35 @@ class MyDocument extends Document {
 }
 
 export default MyDocument;
+
+function Favicon() {
+   return (
+      <>
+         <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-touch-icon.png"
+         />
+         <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png"
+         />
+         <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+         />
+         <link rel="manifest" href="/favicon/site.webmanifest" />
+         <link
+            rel="mask-icon"
+            href="/favicon/safari-pinned-tab.svg"
+            color="#5bbad5"
+         />
+         <meta name="msapplication-TileColor" content="#da532c" />
+         <meta name="theme-color" content="#ffffff" />
+      </>
+   );
+}

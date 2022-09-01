54fdeb4c85566a4ab9ea97a0fd707c75f490de54
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-08-31 18:21:48 -0700

    feat(footer): Make logo a link to homepage

--- packages/common/components/Footer.tsx
+++ packages/common/components/Footer.tsx
@@ -1,5 +1,6 @@
-import { HStack, StackProps } from "@chakra-ui/react";
+import { HStack, Link, StackProps } from "@chakra-ui/react";
 import Image from "next/image";
+import NextLink from "next/link";
 import Favicon from "@public/favicon/android-chrome-512x512.png";
 import { SocialIconRow } from "@icons/social";
 
@@ -8,12 +9,16 @@ type FooterProps = StackProps;
 export function Footer(props: FooterProps) {
    return (
       <HStack justify="space-between" px={8} my={16} {...props}>
-         <Image
-            src={Favicon}
-            alt="Mason's initials (MM) in a circle"
-            width={64}
-            height={64}
-         />
+         <NextLink href="/" passHref>
+            <Link>
+               <Image
+                  src={Favicon}
+                  alt="Mason's initials (MM) in a circle"
+                  width={64}
+                  height={64}
+               />
+            </Link>
+         </NextLink>
          <SocialIconRow />
       </HStack>
    );

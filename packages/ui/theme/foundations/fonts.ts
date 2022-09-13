4a9d21209291a112f2b74da2f3b190d56705e696
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-09-13 09:21:25 -0700

    fix(lint): avoid escape

--- packages/ui/theme/foundations/fonts.ts
+++ packages/ui/theme/foundations/fonts.ts
@@ -1,7 +1,8 @@
 import type { ThemeOverride } from "@chakra-ui/react";
 
 export const fonts: ThemeOverride["fonts"] = {
-   body: "system-ui, \"Segoe UI\", Roboto, Helvetica, Arial, sans-serif, \"Apple Color Emoji\", \"Segoe UI Emoji\", \"Segoe UI Symbol\"",
-   heading: "system-ui, \"Segoe UI\", Roboto, Helvetica, Arial, sans-serif, \"Apple Color Emoji\", \"Segoe UI Emoji\", \"Segoe UI Symbol\"",
+   body: 'system-ui, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+   heading:
+      'system-ui, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
    mono: "ui-monospace, SFMono-Regular, SF Mono, Menlo, Consolas, Liberation Mono, monospace",
 };

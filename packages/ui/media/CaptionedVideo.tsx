e4759af07cd101c17c6b380549c9f1766d8dc359
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2023-09-30 12:17:19 -0700

    refactor(tailwind): captioned video

--- packages/ui/media/CaptionedVideo.tsx
+++ packages/ui/media/CaptionedVideo.tsx
@@ -1,21 +1,18 @@
-import { HStack, Text, VStack } from "@chakra-ui/react";
-
 type CaptionedVideoProps = React.PropsWithChildren<{
    mp4: string;
    webm: string;
 }>;
-
 export function CaptionedVideo({ children, mp4, webm }: CaptionedVideoProps) {
    return (
-      <VStack justify="center" textAlign="center">
-         <HStack width="full" justify="center">
+      <div className="flex flex-col justify-center text-center">
+         <div className="flex w-full justify-center">
             <video autoPlay loop playsInline width="768">
                <source src={webm} type="video/webm" />
                <source src={mp4} type="video/mp4" />
-               <Text>Your browser does not support the video tag 😕</Text>
+               <p>Your browser does not support the video tag 😕</p>
             </video>
-         </HStack>
+         </div>
          {children}
-      </VStack>
+      </div>
    );
 }

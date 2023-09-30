229cf231c11a4c4871af090be2461d0819864476
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2023-09-29 21:41:41 -0700

    fix(csai): add video type

--- packages/ui/media/CaptionedVideo.tsx
+++ packages/ui/media/CaptionedVideo.tsx
@@ -2,14 +2,15 @@ import { Box, HStack, Text, VStack } from "@chakra-ui/react";
 
 type CaptionedVideoProps = React.PropsWithChildren<{
    src: string;
+   type: string;
 }>;
 
-export function CaptionedVideo({ children, src }: CaptionedVideoProps) {
+export function CaptionedVideo({ children, src, type }: CaptionedVideoProps) {
    return (
       <VStack justify="center" textAlign="center">
          <HStack width="full" justify="center">
             <video autoPlay loop playsInline width="768">
-               <source src={src} />
+               <source src={src} type={type} />
             </video>
          </HStack>
          {children}

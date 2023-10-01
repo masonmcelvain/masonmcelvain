d7901156c09115ad9def2998e6b33d39b5855a33
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2023-09-30 17:06:11 -0700

    feat: add dynamic logos

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import "./globals.css";
 import { Wrapper } from "@ui/layout";
-
 import { Footer } from "./Footer";
 import { Providers } from "./providers";
 import { Header } from "./Header";
@@ -9,20 +8,27 @@ type RootLayoutProps = {
    headTitleSuffix: string;
 };
 
-export default function RootLayout({
+export default async function RootLayout({
    children,
 }: React.PropsWithChildren<RootLayoutProps>) {
+   const logoSrc = getRandomLogoPath();
    return (
       <html lang="en">
          <body>
             <Providers>
                <Wrapper>
-                  <Header />
+                  <Header logoSrc={logoSrc} />
                   {children}
-                  <Footer />
+                  <Footer logoSrc={logoSrc} />
                </Wrapper>
             </Providers>
          </body>
       </html>
    );
+}
+
+function getRandomLogoPath() {
+   const max = 24;
+   const int = Math.floor(Math.random() * max + 1);
+   return `/assets/images/logos/logo-${int}.png`;
 }

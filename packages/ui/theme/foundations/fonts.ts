eab5f86cd4054cc3f51875e6d274fae283270637
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2023-09-30 23:46:47 -0700

    feat(tailwind): fonts

--- packages/ui/theme/foundations/fonts.ts
+++
@@ -1,8 +0,0 @@
-import type { ThemeOverride } from "@chakra-ui/react";
-
-export const fonts: ThemeOverride["fonts"] = {
-   body: 'system-ui, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
-   heading:
-      'system-ui, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
-   mono: "ui-monospace, SFMono-Regular, SF Mono, Menlo, Consolas, Liberation Mono, monospace",
-};

3c39b918b810197b6326f5b63cb80c3ad7ed9702
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2023-10-02 12:53:10 -0700

    refactor: drop hero fragment

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -3,24 +3,22 @@ import Image from "next/image";
 
 export function Hero() {
    return (
-      <>
-         <div className="flex w-full flex-col items-center justify-between gap-8 px-4 md:flex-row md:gap-16 lg:px-8 xl:gap-32">
-            <div className="flex flex-col md:space-y-4">
-               <H1>Hi, I&apos;m Mason.</H1>
-               <H1>Full Stack Developer</H1>
-               <H1>at iFixit.</H1>
-            </div>
-            <div className="relative h-[200px] w-[200px] overflow-hidden rounded-full lg:h-[375px] lg:w-[375px]">
-               <Image
-                  src={RockMason}
-                  alt="Mason Climbing in Tuolomne Meadows"
-                  fill
-                  object-fit="cover"
-                  priority
-               />
-            </div>
+      <div className="flex w-full flex-col items-center justify-between gap-8 px-4 md:flex-row md:gap-16 lg:px-8 xl:gap-32">
+         <div className="flex flex-col md:space-y-4">
+            <H1>Hi, I&apos;m Mason.</H1>
+            <H1>Full Stack Developer</H1>
+            <H1>at iFixit.</H1>
          </div>
-      </>
+         <div className="relative h-[200px] w-[200px] overflow-hidden rounded-full lg:h-[375px] lg:w-[375px]">
+            <Image
+               src={RockMason}
+               alt="Mason Climbing in Tuolomne Meadows"
+               fill
+               object-fit="cover"
+               priority
+            />
+         </div>
+      </div>
    );
 }
 

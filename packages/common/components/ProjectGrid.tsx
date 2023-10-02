0cc31daeb353b3be3534d7af54b9a844fda9f208
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2023-10-02 12:26:11 -0700

    fix(tailwind): project card pseudo classes

--- packages/common/components/ProjectGrid.tsx
+++ packages/common/components/ProjectGrid.tsx
@@ -1,25 +1,17 @@
-import { ProjectCard, ProjectCardProps } from "./ProjectCard";
-import * as React from "react";
+import { ProjectCard } from "./ProjectCard";
+import type { ProjectCardProps } from "./ProjectCard";
 
 type ProjectGridProps = {
    projects: ProjectCardProps[];
 };
 
 export function ProjectGrid({ projects }: ProjectGridProps) {
-   const lastChildClass = React.useCallback(
-      (index: number) => {
-         const isOddLastChild =
-            projects.length % 2 && index === projects.length - 1;
-         return isOddLastChild ? "lg:col-start-2 lg:col-end-4" : "";
-      },
-      [projects.length],
-   );
    return projects.length ? (
       <div className="mt-12 grid grid-cols-[repeat(1,1fr)] gap-8 lg:grid-cols-[repeat(4,1fr)]">
          {projects.map((props, index) => (
             <div
                key={index}
-               className={`col-span-1 lg:col-span-2 ${lastChildClass(index)}`}
+               className="col-span-1 lg:col-span-2 last:odd:lg:col-start-2 last:odd:lg:col-end-4"
             >
                <ProjectCard {...props} />
             </div>

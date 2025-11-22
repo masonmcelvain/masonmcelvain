bb0fa7ce0731a35b7190e93231c5b5ceb7e64a2d
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2025-11-22 09:51:47 -0700

    delete code components

--- app/code/hop/page.tsx
+++
@@ -1,5 +0,0 @@
-import { getMetadata } from "@data/metadata";
-import type { Metadata } from "next";
-
-export const metadata: Metadata = getMetadata({ suffix: "Hop" });
-export { default } from "./Hop";
--- app/code/ifixit/page.tsx
+++
@@ -1,5 +0,0 @@
-import { getMetadata } from "@data/metadata";
-import type { Metadata } from "next";
-
-export const metadata: Metadata = getMetadata({ suffix: "iFixit" });
-export { default } from "./iFixit";

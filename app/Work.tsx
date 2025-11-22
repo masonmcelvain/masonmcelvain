c49dfe45ac412f3d91ae8a86317aa575e503e406
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2025-11-22 10:06:59 -0700

    drop deeper dive text

--- app/Work.tsx
+++ app/Work.tsx
@@ -2,10 +2,6 @@ export function Work() {
    return (
       <div className="flex flex-col space-y-8">
          <h2>What I&apos;ve been working on 🛠️</h2>
-         <p>
-            A deeper dive into some of the projects and teams I&apos;ve devoted
-            my time to.
-         </p>
       </div>
    );
 }

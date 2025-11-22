03d0a6efb2e90ee56ff1e4083c39baeb0612bf22
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2025-11-22 10:14:44 -0700

    drop markdown text

--- app/App.tsx
+++ app/App.tsx
@@ -1,20 +1,21 @@
 import { ProjectGrid } from "@common/components";
 import projects from "@data/projects";
-import { About } from "./About";
 import { Collage } from "./Collage";
 import { Hero } from "./Hero";
 import { MobileCollage } from "./MobileCollage";
-import { Work } from "./Work";
 
 export default function App() {
    return (
       <div className="flex flex-col space-y-8">
          <Hero />
-         <About />
-         <>
-            <Work />
-            <ProjectGrid projects={projects} />
-         </>
+         <div className="flex flex-col space-y-8">
+            <p>
+               Currently based in Salt Lake City. I graduated from Cal Poly San
+               Luis Obispo with a degree in computer science in March 2022.
+            </p>
+         </div>
+         <h2 className="mb-12">What I&apos;ve been working on 🛠️</h2>
+         <ProjectGrid projects={projects} />
          <hr aria-orientation="horizontal" className="my-16 border-gray-200" />
          <Collage />
          <MobileCollage />
--- app/Work.tsx
+++
@@ -1,7 +0,0 @@
-export function Work() {
-   return (
-      <div className="flex flex-col space-y-8">
-         <h2>What I&apos;ve been working on 🛠️</h2>
-      </div>
-   );
-}

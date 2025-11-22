ce61bb850d880b9e9a00fd655d40730f29b6bd10
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2025-11-22 10:37:51 -0700

    drop extra div

--- app/App.tsx
+++ app/App.tsx
@@ -7,12 +7,10 @@ export default function App() {
    return (
       <div className="flex flex-col space-y-8">
          <Hero />
-         <div className="flex flex-col space-y-8">
-            <p>
-               Currently based in Salt Lake City. I graduated from Cal Poly San
-               Luis Obispo with a degree in computer science in March 2022.
-            </p>
-         </div>
+         <p>
+            Currently based in Salt Lake City. I graduated from Cal Poly San
+            Luis Obispo with a degree in computer science in March 2022.
+         </p>
          <h2 className="mb-12">What I&apos;ve been working on 🛠️</h2>
          <ProjectGrid projects={projects} />
          <hr aria-orientation="horizontal" className="my-16 border-gray-200" />

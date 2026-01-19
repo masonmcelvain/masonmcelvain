436765c7fa127be0183f2c1ca2a8434eabcf0a30
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2026-01-19 11:53:25 -0800

    fix: mason's blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -13,8 +13,8 @@ export default function BlogPage() {
 
    return (
       <>
-         <h1>Blog</h1>
-         <ul className="mt-4 space-y-6">
+         <h1>Mason&apos;s blog</h1>
+         <ul className="mt-8 space-y-6">
             {posts.map((post) => (
                <li key={post.slug}>
                   <Link

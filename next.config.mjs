70eb275277a8ae2ea7e858ed1c56ecefa6daa142
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2026-01-21 09:47:16 -0700

    fix: cleanup next config

--- next.config.mjs
+++ next.config.mjs
@@ -4,18 +4,10 @@ const nextConfig = {
       remotePatterns: [
          {
             protocol: "https",
-            hostname: "lh3.googleusercontent.com",
-            port: "",
-            pathname:
-               "/drive-viewer/AK7aPaCTGHNVVx5V2BRX78sRQjGSkkR49dgQ9vvt_PX7wgB1uHE7DvL-cI5GX1Yc0F7nUsW4Ha9zyY3piegQwbLjGSN_OWi8iw=w1920-h1058",
-         },
-         {
-            protocol: "https",
             hostname: "media.masonmcelvain.com",
          },
       ],
    },
-   pageExtensions: ["ts", "tsx", "js", "jsx", "mdx"],
 };
 
 export default nextConfig;

c8bb94a400070ab053f9d1b581a07ade026a350b
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2026-05-30 12:34:19 -0600

    refactor: hero layout

--- app/page.tsx
+++ app/page.tsx
@@ -13,12 +13,6 @@ export default function Page() {
    return (
       <main className="flex flex-col space-y-8">
          <Hero />
-         <p>
-            Currently I&apos;m based in Salt Lake City, where I build
-            iFixit&apos;s e-commerce storefront and tooling. I graduated from
-            Cal Poly San Luis Obispo with a computer science degree in March
-            2022.
-         </p>
          <SocialIconRow className="mb-12 self-center md:hidden" />
          <h2 className="mb-12 text-center">Me on the web</h2>
          <ProjectGrid projects={projects} />

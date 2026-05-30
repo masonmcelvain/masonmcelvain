fd50cb288817662dbb278a7527c56c7af592ce7d
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2026-05-30 13:00:56 -0600

    refactor: blog link text layout

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -19,7 +19,7 @@ export default function BlogPage() {
                <li key={post.slug}>
                   <Link
                      href={`/blog/${post.slug}`}
-                     className="group border-border hover:border-border-hover flex flex-col gap-4 rounded-lg border transition-colors sm:flex-row sm:items-start"
+                     className="group border-border hover:border-border-hover flex flex-col rounded-lg border transition-colors sm:flex-row sm:items-center"
                   >
                      <div className="relative aspect-[4/3] w-full shrink-0 overflow-hidden rounded-tl-lg rounded-tr-lg sm:w-48 sm:rounded-tr-none sm:rounded-bl-lg">
                         <Image
@@ -30,7 +30,7 @@ export default function BlogPage() {
                            sizes="(max-width: 640px) 100vw, 192px"
                         />
                      </div>
-                     <div className="flex-1">
+                     <div className="flex-1 p-4">
                         <h2 className="text-foreground text-xl font-semibold">
                            {post.title}
                         </h2>
